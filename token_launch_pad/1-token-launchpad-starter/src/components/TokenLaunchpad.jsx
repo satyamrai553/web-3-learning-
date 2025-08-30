38747484b64664a841aec99d6ebdb1163abdc8d9
Satyam Rai <satyamrai553@gmail.com>
2025-08-30 17:58:17 +0530

    token_launch_pad completed

--- token_launch_pad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ token_launch_pad/1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,5 +1,45 @@
+import { getMinimumBalanceForRentExemptMint } from "@solana/spl-token";
+import {TOKEN_PROGRAM_ID,createInitializeMint2Instruction } from "@solana/spl-token";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Connection, Keypair, SystemProgram, Transaction } from "@solana/web3.js";
+import { useState } from "react"
+
 
 export function TokenLaunchpad() {
+    const {connection} = useConnection();
+    const wallet = useWallet();
+    const [name, setName] = useState("");
+    const [symbol, setSymbol] = useState("");
+    const [imageURL, setImageURL] = useState("");
+    const [initialSupply, setInitialSupply] = useState("");
+
+
+
+    async function createToken(){
+        const lamports = await getMinimumBalanceForRentExemptMint(connection)
+        const keypair = Keypair.generate()
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: keypair.publicKey,
+                space: 82,
+                lamports,
+                programId: TOKEN_PROGRAM_ID
+            }),
+            await createInitializeMint2Instruction(keypair.publicKey, 6, wallet.publicKey, wallet.publicKey,TOKEN_PROGRAM_ID)
+        )
+
+        transaction.feePayer = wallet.publicKey;
+        const recentBlockhash = await connection.getLatestBlockhash();
+        transaction.recentBlockhash = recentBlockhash.blockhash;
+
+        transaction.partialSign(keypair);
+        const response = await wallet.sendTransaction(transaction, connection)
+
+        console.log(response)
+    }
+
+
     return  <div style={{
         height: '100vh',
         display: 'flex',
@@ -8,10 +48,10 @@ export function TokenLaunchpad() {
         flexDirection: 'column'
     }}>
         <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+        <input className='inputText' type='text' placeholder='Name' onChange={e=>setName(e.target.value)}></input> <br />
+        <input className='inputText' type='text' placeholder='Symbol' onChange={e=>setSymbol(e.target.value)}></input> <br />
+        <input className='inputText' type='text' placeholder='Image URL' onChange={e=>setImageURL(e.target.value)}></input> <br />
+        <input className='inputText' type='text' placeholder='Initial Supply' onChange={e=>setInitialSupply(e.target.value)}></input> <br />
+        <button onClick={createToken} className='btn'>Create a token</button>
     </div>
 }
